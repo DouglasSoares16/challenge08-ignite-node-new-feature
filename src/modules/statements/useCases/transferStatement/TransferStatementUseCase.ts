d5d1fbd4ac120c4329a08b4719b5e4f00e02c011
Douglas Soares <douglasmaismcpe@gmail.com>
2021-12-13 16:59:18 -0300

    Created use case of TransferStatement

---
+++ src/modules/statements/useCases/transferStatement/TransferStatementUseCase.ts
@@ -0,0 +1,59 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../../users/repositories/IUsersRepository";
+import { OperationType } from "../../entities/Statement";
+import { IStatementsRepository } from "../../repositories/IStatementsRepository";
+import { TransferStatementError } from "./TransferStatementError";
+
+interface IRequest {
+  receiver_id: string;
+  sender_id: string;
+  amount: number;
+  description: string;
+}
+
+@injectable()
+class TransferStatementUseCase {
+  constructor(
+    @inject("StatementsRepository")
+    private statementsRepository: IStatementsRepository,
+
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ sender_id, receiver_id, amount, description }: IRequest) {
+    if (sender_id === receiver_id) {
+      throw new TransferStatementError.IncorrectTransfer();
+    }
+
+    const userReceiver = await this.usersRepository.findById(receiver_id);
+
+    if (!userReceiver) {
+      throw new TransferStatementError.UserReceiverNotFound();
+    }
+
+    const userSenderBalance = await this.statementsRepository.getUserBalance({ user_id: sender_id });
+
+    if (amount > userSenderBalance.balance) {
+      throw new TransferStatementError.InsufficientFunds();
+    }
+
+    await this.statementsRepository.create({
+      user_id: sender_id,
+      type: OperationType.WITHDRAW,
+      amount,
+      description: `Transfered ${amount} to ${receiver_id}`
+    });
+
+    const transfer = await this.statementsRepository.create({
+      user_id: receiver_id,
+      type: OperationType.TRANSFER,
+      amount,
+      description
+    });
+
+    return transfer;
+  }
+}
+
+export { TransferStatementUseCase };
